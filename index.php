d86faf518049e44c5f16b33762c56ca72e97c199
lana <lanatriforce1234@gmail.com>
2024-01-28 13:59:01 -0300

    .

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<title>Página principal | Belo Broche</title> <!-- Título do site -->
 
     <link rel="stylesheet" type="text/css" href="lib/css/index.css"/> <!-- css do index -->
+    <link rel="stylesheet" type="text/css" href="lib/css/zero_page.css"/> <!-- css para zerar margens -->
 </head>
     <!-- Estrutura do site/Conteúdo do site -->
 	<body>
