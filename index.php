e5f91f376ba4f4e4975ea8406636d7d79b1858b7
lana <lanatriforce1234@gmail.com>
2024-01-28 12:58:51 -0300

    .

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" /> <!-- Torna o site na linguagem pt-BR (português - Brasil), sendo possível utilizar acentos -->
+	<title>Página principal | Belo Broche</title> <!-- Título do site -->
+
+    <link rel="stylesheet" type="text/css" href="lib/css/index.css"/> <!-- css do index -->
+</head>
+    <!-- Estrutura do site/Conteúdo do site -->
+	<body>
+	</body>
+    <!-- 
+    http://localhost/belo_broche/index.php
+    http://127.0.0.1/belo_broche/index.php
+    -->
+</html>
